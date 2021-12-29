6ac2fbfa9935bfb4503026d1910376ea5760bf78
mtgha223344 <58712885+mtgha223344@users.noreply.github.com>
2021-12-29 11:30:49 -0500

    Update game.py

--- game.py
+++ game.py
@@ -11,15 +11,21 @@ class GameBoard():
         #nums = [1,2,3,4,5,6,7,8,9]
         i = 0
         for x in range(3):
-            print("| " + str(self.nums[i]) + " |" + "| " + str(self.nums[i] + 1) + " |" "| " + str(self.nums[i] + 2) + " |" )
+            print("| " + str(self.nums[i]) + " |" + "| " + str(self.nums[i]) + " |" "| " + str(self.nums[i]) + " |" )
             print("-" * 15)
             i += 2
     
     def append_choice(self, choice):
+        if choice >= 1 and choice <= 9:
+            self.nums.insert(choice, 'X')
+        else: 
+            print("Nope")
+        self.print_game_board(0)
         
             
 game = GameBoard(0)
 game.print_game_board(0)
 
 print("Let's play a game. Please enter your choice")
-choice = input()
+choice = int(input())
+game.append_choice(choice)
